423abfd546d17038e239c75cbf8b2614911c276d
akeembaik <haqimbeat@gmail.com>
2022-02-26 15:25:57 +0700

    userbot

---
+++ userbot/clients/logger.py
@@ -0,0 +1,74 @@
+# Man - UserBot
+# Copyright (c) 2022 Man-Userbot
+# Credits: @mrismanaziz || https://github.com/mrismanaziz
+#
+# This file is a part of < https://github.com/mrismanaziz/Man-Userbot/ >
+# t.me/YOPIBOT404 & t.me/chyopi
+
+from telethon.tl.functions.channels import InviteToChannelRequest
+
+from userbot import BOT_USERNAME
+from userbot import BOT_VER as version
+from userbot import BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import MAN2, MAN3, MAN4, MAN5, bot, branch
+
+MSG_ON = """
+🔥 **YOPIBOT TELAH AKTIF**
+━━
+➠ **VERSION-** `{}@{}`
+➠ **TYPING** `{}alive` **UNTUK CEK BOT NYE**
+━━
+"""
+
+
+async def man_userbot_on():
+    try:
+        if bot:
+            if BOTLOG_CHATID != 0:
+                await bot.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN2:
+            if BOTLOG_CHATID != 0:
+                await MAN2.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN3:
+            if BOTLOG_CHATID != 0:
+                await MAN3.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN4:
+            if BOTLOG_CHATID != 0:
+                await MAN4.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        if MAN5:
+            if BOTLOG_CHATID != 0:
+                await MAN5.send_message(
+                    BOTLOG_CHATID,
+                    MSG_ON.format(version, branch, cmd),
+                )
+    except BaseException:
+        pass
+    try:
+        await bot(InviteToChannelRequest(int(BOTLOG_CHATID), [BOT_USERNAME]))
+    except BaseException:
+        pass
